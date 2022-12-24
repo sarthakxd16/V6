bcd888f1f7077747a1bc6eea535f9c239e98dfc6
Sarthak Kale <83166658+sarthakkale16@users.noreply.github.com>
2022-12-24 08:57:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5.git /Auto-Filter-V5
+  git clone https://github.com/sarthakkale16/V6.git /V6
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-Filter-V5
 fi
-cd /Auto-Filter-V5
+cd /V6
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
+https://github.com/sarthakkale16/V6
