097a643326773041ec34fa31b457a1bf2667f528
Sarthak (Pricisely) <99407359+sarthakxd16@users.noreply.github.com>
2022-12-27 10:04:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sarthakkale16/V6.git /V6
+  git clone https://github.com/sarthakxd16/V6.git /V6
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /V6
@@ -10,4 +10,4 @@ cd /V6
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
-https://github.com/sarthakkale16/V6
+https://github.com/sarthakxd16/V6
